2e7b8c237d3dd19c8a385e6202bc3055dad996fd
caiovictormc <caiovictormc@gmail.com>
2019-01-07 15:07:55 -0300

    ADDED: Starting to create test_sqlite3

--- sentinel/tests/test_database.py
+++
@@ -1,6 +0,0 @@
-from sentinel.database import DBManager
-
-
-def test_create_manager():
-    manager = DBManager("postgres://USER:PASSWORD@HOST:PORT/NAME")
-    assert isinstance(manager, DBManager)
---
+++ sentinel/tests/test_db_services/test_sqlite3.py
@@ -0,0 +1,36 @@
+from sentinel.database import manager
+from sentinel.database.services import SQlite3
+
+import pytest
+
+
+DB_NAME = ':memory:'
+
+
+@pytest.fixture(scope="module")
+def db_service(request):
+    db = manager(f"sqlite://{DB_NAME}")
+
+    def fin():
+        db.close()
+    request.addfinalizer(fin)
+    return db
+
+
+def test_is_sqlite_instance(db_service):
+    assert isinstance(db_service, SQlite3)
+
+
+def test_create_the_rule_table(db_service):
+    db_service.migrate()
+
+    cursor = db_service.conn.cursor()
+    query = cursor.execute("""
+        SELECT name FROM sqlite_master WHERE type='table' AND name='rules';
+    """)
+    results = query.fetchone()
+    assert len(results) > 0
+
+
+def test_add_rules(db_service):
+    db_service.add_rule()
