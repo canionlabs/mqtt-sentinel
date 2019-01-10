c777b8b600099c3ea8e7e83bc0ca3042d57f12e8
caiovictormc <caiovictormc@gmail.com>
2019-01-10 20:06:03 -0300

    REMOVED: class MQTTClient from sentinel

--- sentinel/sentinel.py
+++ sentinel/sentinel.py
@@ -1,28 +1,5 @@
 # coding: utf-8
-import paho.mqtt.client as mqtt
 from sentinel.database import manager
-
-
-class MQTTClient:
-    def __init__(self, host, port, keepalive, username=None, password=None):
-        self.host = host
-        self.port = port or 1883
-        self.keepalive = keepalive
-        self.username = username
-        self.password = password
-
-        self._mqttc = mqtt.Client()
-        self._set_credencials()
-
-    def _set_credencials(self):
-        if self.username:
-            if self.password:
-                self._mqttc.username_pw_set(self.username, self.password)
-            else:
-                self._mqttc.username_pw_set(self.username)
-
-    def connect(self):
-        self._mqttc.connect(self.host, self.port, self.keepalive)
 
 
 class Sentinel:
