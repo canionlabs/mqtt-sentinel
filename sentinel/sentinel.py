91a58506201cf537eb258c0015c37675702869a8
caiovictormc <caiovictormc@gmail.com>
2019-01-16 15:46:19 -0300

    CHANGED: Setinel to receive mqtt params

--- sentinel/sentinel.py
+++ sentinel/sentinel.py
@@ -1,24 +1,32 @@
 # coding: utf-8
+import logging
+
 from sentinel.database import manager
 from sentinel.watcher import Watcher
 from sentinel import settings
 
 
 class Sentinel:
-    def __init__(self):
+    def __init__(self, host='localhost', port=1883, keepalive=60):
         self.db = None
-        self.watcher = Watcher()
+        self.watcher = Watcher(host, port, keepalive)
+
+    def set_mqtt_auth(self, username, password):
+        self.watcher.set_auth(username, password)
 
     def set_output(self, output_service):
         settings.output_service = output_service
 
-    def set_db(self, db_url):
+    def set_db(self, db_url='sqlite://sentinel.db'):
         self.db = manager(db_url)
         self.db.migrate()
         settings.db_service = self.db
 
     def add_rule(self, mqtt_rule):
-        self.db.add_rule(mqtt_rule)
+        if not self.db.this_rule_exists(mqtt_rule):
+            self.db.add_rule(mqtt_rule)
+        else:
+            logging.warning('This rule already exists')
 
     def start(self):
         self._checkup()
