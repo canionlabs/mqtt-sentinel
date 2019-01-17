9ca768be6f3dd394a7830d17448659e88197ff62
caiovictormc <caiovictormc@gmail.com>
2019-01-17 10:37:45 -0300

    CHANGED: merging RuleDBObject and Rule and creating tests

--- sentinel/rules.py
+++ sentinel/rules.py
@@ -1,31 +1,41 @@
 # coding: utf-8
 
 
-class RuleDBObject:
-    """
-    Rule Object used to represent DB Rules received from Database
-    """
-    def __init__(self, topic, operator="!=", equated=""):
-        self.topic = topic
-        self.operator = operator
-        self.equated = equated
-
-
 class Rule:
     """
     Rule Object used to represent DB Rules received from Database
     """
     def __init__(self, topic, operator="!=", equated=""):
-        self.topic = topic
-        self.operator = operator
-        self.equated = equated
+        self._topic = topic
+        self._operator = operator
+        self._equated = equated
 
-    def set_operation(self, operator, equated):
-        self.operator = operator or '!='
-        self.equated = equated or ''
+    @property
+    def topic(self):
+        return self._topic
+
+    @topic.setter
+    def topic(self, topic_name):
+        self._topic = topic_name
+
+    @property
+    def operator(self):
+        return self._operator
+
+    @operator.setter
+    def operator(self, operator_type):
+        self._operator = operator_type
+
+    @property
+    def equated(self):
+        return self._equated
+
+    @equated.setter
+    def equated(self, equated_value):
+        self._equated = equated_value
 
     def __str__(self):
-        return f'{self.topic}, {self.operation}, {self.equated}'
+        return f'{self.topic} {self.operator} {self.equated}'
 
 
 class HomieRule:
